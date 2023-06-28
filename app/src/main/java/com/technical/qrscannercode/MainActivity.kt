010858ea902f38614ab3f1cf72250db27cceadf6
damac53 <mitchellefetty@gmail.com>
2023-06-28 15:29:42 +0200

    QR Scanner up and running

--- app/src/main/java/com/technical/qrscannercode/MainActivity.kt
+++ app/src/main/java/com/technical/qrscannercode/MainActivity.kt
@@ -2,10 +2,33 @@ package com.technical.qrscannercode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.view.Surface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.MaterialTheme
+import java.lang.reflect.Modifier
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        QRScanner3Theme {
+            Surface(modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colors.background) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Layout.Alignment.Cen
+                            terHorizontally
+                ) {
+                    PreviewViewComposable()
+                    Text(
+                        text = "Scan QR Code",
+                        modifier = Modifier.padding(top = 48.dp)
+                    )
+
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/technical/qrscannercode/PreviewViewComposable.kt
@@ -0,0 +1,66 @@
+package com.technical.qrscannercode
+
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import java.lang.reflect.Modifier
+import java.util.concurrent.Executors
+
+@androidx.camera.core.ExperimentalGetImage
+
+fun PreviewViewComposable() {
+    AndroidView({ context ->
+        val cameraExecutor = Executors.newSingleThreadExecutor()
+        val previewView = PreviewView(context).also {
+            it.scaleType = PreviewView.ScaleType.FILL_CENTER
+        }
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+            val imageCapture = ImageCapture.Builder().build()
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, BarcodeAnalyser{
+                        Toast.makeText(context, "Barcode found", Toast.LENGTH_SHORT).show()
+                    })
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    context as ComponentActivity, cameraSelector, preview, imageCapture, imageAnalyzer)
+
+            } catch(exc: Exception) {
+                Log.e("DEBUG", "Use case binding failed", exc)
+            }
+        }, ContextCompat.getMainExecutor(context))
+        previewView
+    },
+        modifier = Modifier
+            .size(width = 250.dp, height = 250.dp))
+}
+
+class AndroidView(any: Any, modifier: Any) {
+
+}
